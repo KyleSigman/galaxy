ad426ded3ca795c4cad874bb4422271b5ffc428c
bmc <sigmankyle88@gmail.com>
2026-03-20 10:33:31 +0400

    Update server.js

--- server.js
+++ server.js
@@ -1,3 +1,8 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+app.use(express.static(path.join(__dirname, 'build')));
+
 
 const WebSocket = require('ws');
 const port = process.env.PORT || 8080;
